dc8a6851919c65679fed36c5ef01e619812a7e0a
Nebiyu Daniel <nebiyu.daniel@stu.ocsb.ca>
2025-09-16 18:00:46 +0000

    done

---
+++ basicMath.cpp
@@ -0,0 +1,16 @@
+// Created by: Nebiyu Daniel
+// Created on: september. 16, 2025
+// This program does basic math.
+// Copyright 2025 Nebiyu Daniel
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "12 + 4 = " << 12 + 4 << std::endl;
+    std::cout << "3 - 1 = " << 3 - 1 << std::endl;
+    std::cout << "49 / 7 = " << 49 / 7 << std::endl;
+    std::cout << "24 - 12 / 6 = " << 24 - 12 / 6 << std::endl;
+    std::cout << "4^6 = " << pow(4, 6) << std::endl;
+    std::cout << "3 / 9 = " << float(3) / float(9) << std::endl;
+}
